02952639f56bca1ad4d24c615d7d602d5f8e7312
tealeave <tealeave@gmail.com>
2023-08-10 10:54:11 -0700

    job updated

--- gradient_job.sh
+++ gradient_job.sh
@@ -3,11 +3,10 @@
 #SBATCH --job-name=gradient_boost_job   ## job name
 #SBATCH -p free                         ## use free partition
 #SBATCH --nodes=1                       ## use 1 node, don't ask for multiple
-#SBATCH --ntasks=4                      ## ask for 4 CPU
-#SBATCH --mem-per-cpu=8G                ## ask for 8Gb memory per CPU
+#SBATCH --ntasks=8                      ## ask for 8 CPU
+#SBATCH --mem-per-cpu=4G                
 #SBATCH --error=%x.%A.err               ## Slurm error  file, %x - job name, %A job id
 #SBATCH --out=%x.%A.out                 ## Slurm output file, %x - job name, %A job id
-#SBATCH -t 2:00:00                      ## 2 hr run time limit
 
 
 # load python module
